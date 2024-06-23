760d62e2c14f1a46b44f314be92787da99ced64e
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-06-23 15:20:25 -0300

    delete: removendo arquivos sem uso

--- components/MenuMobile.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MenuMobile() {
-  return (
-    <div className="flex lg:hidden lg:flex-1 lg:items-center lg:justify-end lg:space-x->
-
-    </div>
-  )
-}
